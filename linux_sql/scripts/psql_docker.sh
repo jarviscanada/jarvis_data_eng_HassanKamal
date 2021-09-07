f4053206871906d27f3cd22a3f7b865a6c83a7c5
Hassan Kamal <hassan.m.kamal4@gmail.com>
2021-09-07 15:51:33 +0000

    Completed psql_docker script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,48 +1,55 @@
 #! /bin/bash
 
+#setup 3 arguments to variables
 cmd=$1
 db_username=$2
 db_password=$3
 
+#start docker server if not running
 sudo systemctl status docker || systemctl start docker
 
+#switch cases for possible options for docker; create, start, stop
 case $cmd in
 
+  #create a docker container
   create)
+    #check whether container has already been created
     if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" = "2" ]; then
-      echo "jrvs-psql container has already been created" >&2
+      echo "Error: \"jrvs-psql\" container has already been created" >&2
       exit 1
     fi
 
+    #check whether the database username and password have been given as arguments
     if [ "$#" -ne 3 ]; then
-      echo "db_username or db_password not passed as arguments" >&2
+      echo "Error: db_username or db_password not passed as arguments" >&2
       exit 1
     fi
 
+    #create volume then container using psql image
     docker volume create pgdata
     docker run --name jrvs-psql -e POSTGRES_PASSWORD=${db_password} -e POSTGRES_USER=${db_username} -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
     exit $?
     ;;
+
+  #start or stop the container
+  start | stop)
+    #check whether the conatiner has been created before starting and stopping
+    if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" != "2" ]; then
+      echo "Error: \"jrvs-psql\" container has not been created" >&2
+      exit 1
+    fi
+
+    #start or stop the container based of the argument provided
+    docker container "$cmd" jrvs-psql
+    exit $?
+    ;;
+
+  #deal with invalid option
+  *)
+    #error message for invalid option
+    echo "Error: invalid option for docker. either choose create, start or stop"
+    exit 1
+    ;;
 esac
 
-if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" != "2" ]; then
-  echo "container failed to create" >&2
-  exit 1
-fi
-
-if [ $1 = "start" ]; then
-  docker container start jrvs-psql
-  exit $?
-fi
-
-if [ $1 = "stop" ]; then
-  docker container stop jrvs-psql
-  exit $?
-fi
-
-if [[ $1 != "create" && $1 != "start" && $1 != "stop" ]]; then
-  echo "Invalid option for docker. Either choose create, start or stop" >&2
-  exit 1
-fi
-
-exit 0
+exit 1
