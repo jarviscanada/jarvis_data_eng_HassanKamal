db22e0f16ea984cb0f40f97811e0827fb5190fee
Hassan Kamal <hassan.m.kamal4@gmail.com>
2021-09-07 16:04:48 +0000

    Fixed the psql_docker script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -8,13 +8,15 @@ db_password=$3
 #start docker server if not running
 sudo systemctl status docker || systemctl start docker
 
+#varaible to check if docker container is created
+container_wc=$(docker container ls -a -f name=jrvs-psql | wc -l)
+
 #switch cases for possible options for docker; create, start, stop
 case $cmd in
-
   #create a docker container
   create)
     #check whether container has already been created
-    if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" = "2" ]; then
+    if [ "$container_wc" = "2" ]; then
       echo "Error: \"jrvs-psql\" container has already been created" >&2
       exit 1
     fi
@@ -33,8 +35,8 @@ case $cmd in
 
   #start or stop the container
   start | stop)
-    #check whether the conatiner has been created before starting and stopping
-    if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" != "2" ]; then
+    #check whether the container has been created before starting and stopping
+    if [ "$container_wc" != "2" ]; then
       echo "Error: \"jrvs-psql\" container has not been created" >&2
       exit 1
     fi
@@ -47,7 +49,7 @@ case $cmd in
   #deal with invalid option
   *)
     #error message for invalid option
-    echo "Error: invalid option for docker. either choose create, start or stop"
+    echo "Error: invalid option for docker. either choose create, start or stop" >&2
     exit 1
     ;;
 esac
